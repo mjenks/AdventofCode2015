95400d0dd94344a2d1089607862a71837f1da9f1
mjenks <rikku.hotaru@gmail.com>
2022-01-10 20:15:31 -0500

    day9 set up
    input for day 9 and structured code with input parsing

---
+++ day9.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 10 20:05:45 2022
+
+@author: mjenks
+"""
+
+#parse input
+def parse(puzzle_input):
+    data = []
+    for line in puzzle_input:
+        line = line.strip()
+        path, distance = line.split('=')
+        cities = path.split('to')
+        trip = cities, distance
+        data.append(trip)
+    return data
+    
+
+#functions for part 1
+
+#solve part 1
+def part1(puzzle_data):
+    return 0
+
+#functions for part 2
+
+#solve part 2
+def part2(puzzle_data):
+    return 0
+
+#run and print solution 
+puzzle_path = "input_day9.txt"
+with open(puzzle_path) as f:
+    puzzle_input = f.readlines()
+    
+puzzle_data = parse(puzzle_input)
+solution1 = part1(puzzle_data)
+solution2 = part2(puzzle_data)
+print(solution1)
+print(solution2)
