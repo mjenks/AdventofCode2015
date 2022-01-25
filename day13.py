71bd57073186c7debd371fe76a08070a8b1bfd1f
mjenks <rikku.hotaru@gmail.com>
2022-01-25 11:13:30 -0500

    parse input
    parsesed text input

--- day13.py
+++ day13.py
@@ -7,6 +7,25 @@ Created on Tue Jan 25 10:24:29 2022
 
 #parse input
 def parse(puzzle_input):
+    data = [line.split() for line in puzzle_input]
+    #make a list of the guests
+    people = set()
+    for line in data:
+        people.add(line[0])
+    people = list(people)
+    #make a matrix of the happiness of each seat pairing
+    happiness = [[0 for i in range(len(people))] for j in range(len(people))]
+    for line in data:
+        x = people.index(line[0]) 
+        person2 = ''.join(list(line[-1])[:-1])
+        y = people.index(person2)
+        if line[2] == 'lose':
+            value = -1 * int(line[3])
+        else:
+            value = int(line[3])
+        happiness[x][y] = value
+
+    data = people, happiness
     return data
     
 
@@ -25,7 +44,7 @@ def part2(puzzle_data):
 #run and print solution 
 puzzle_path = "input_day13.txt"
 with open(puzzle_path) as f:
-    puzzle_input = f.read()
+    puzzle_input = f.readlines()
     
 puzzle_data = parse(puzzle_input)
 solution1 = part1(puzzle_data)
