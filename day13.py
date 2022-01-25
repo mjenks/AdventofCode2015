0b218ef1876ff416562bbe8df2f8cc7f15e5443d
mjenks <rikku.hotaru@gmail.com>
2022-01-25 12:00:12 -0500

    part 1 solved
    code to brute force a solution for part 1

--- day13.py
+++ day13.py
@@ -5,6 +5,8 @@ Created on Tue Jan 25 10:24:29 2022
 @author: mjenks
 """
 
+import itertools
+
 #parse input
 def parse(puzzle_input):
     data = [line.split() for line in puzzle_input]
@@ -33,7 +35,24 @@ def parse(puzzle_input):
 
 #solve part 1
 def part1(puzzle_data):
-    return 0
+    people, happiness = puzzle_data
+    arrangements = list(itertools.permutations(people))
+    max_mood = 0
+    for seating in arrangements:
+        mood = 0
+        for i in range(len(seating)-1):
+            a = people.index(seating[i])
+            b = people.index(seating[i+1])
+            mood += happiness[a][b] + happiness[b][a]
+        a = people.index(seating[-1])
+        b = people.index(seating[0])
+        mood += happiness[a][b] + happiness[b][a]
+        
+        if mood > max_mood:
+            max_mood = mood
+            
+    
+    return max_mood
 
 #functions for part 2
 
