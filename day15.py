1d1146ec4ec997cdb7608a822a2999685645cba3
mjenks <rikku.hotaru@gmail.com>
2022-01-28 15:03:58 -0500

    set up
    set up structure for day 15 solution code and added input file

---
+++ day15.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 28 14:51:21 2022
+
+@author: mjenks
+"""
+
+#define an ingredient class
+class ingredient:
+    def _init_(self, cap, dur, fla, tex, cal):
+        self.capacity = cap
+        self.durability = dur
+        self.flavor = fla
+        self.texture = tex
+        self.calories = cal
+
+#parse input
+def parse(puzzle_input):
+    return data
+    
+
+#functions for part 1
+
+#solve part 1
+def part1(puzzle_data):
+    return 0
+
+#functions for part 2
+
+#solve part 2
+def part2(puzzle_data):
+    return 0
+
+#run and print solution 
+puzzle_path = "input_day15.txt"
+with open(puzzle_path) as f:
+    puzzle_input = f.read()
+    
+puzzle_data = parse(puzzle_input)
+solution1 = part1(puzzle_data)
+solution2 = part2(puzzle_data)
+print(solution1)
+print(solution2)
