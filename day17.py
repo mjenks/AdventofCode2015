90aaa46dc9f4cc54275c8a6f5c17ea004287f0ad
mjenks <rikku.hotaru@gmail.com>
2022-02-04 20:28:08 -0500

    set up
    outline code for day 17 solutions

---
+++ day17.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 04 20:26:42 2022
+
+@author: mjenks
+"""
+
+#parse input
+def parse(puzzle_input):
+    return data
+    
+
+#functions for part 1
+
+#solve part 1
+def part1(puzzle_data):
+    return 0
+
+#functions for part 2
+
+#solve part 2
+def part2(puzzle_data):
+    return 0
+
+#run and print solution 
+puzzle_path = "input_day17.txt"
+with open(puzzle_path) as f:
+    puzzle_input = f.readlines()
+    
+puzzle_data = parse(puzzle_input)
+solution1 = part1(puzzle_data)
+solution2 = part2(puzzle_data)
+print(solution1)
+print(solution2)
