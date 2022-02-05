012bd5bdf9322a823326ec330ddede05ac402036
mjenks <rikku.hotaru@gmail.com>
2022-02-05 16:36:00 -0500

    update function
    added a function which takes a grid and returns the grid at the next
    step

--- day18.py
+++ day18.py
@@ -5,21 +5,65 @@ Created on Sat Feb 05 15:59:29 2022
 @author: mjenks
 """
 # # is on . is off
-# use 1 for on and 0 for off in parsed data
+# 0 for off 1 for on in data
 #parse input
 def parse(puzzle_input):
     data = []
     for line in puzzle_input:
-        row = list(line.strip())
+        row_symb = list(line.strip())
+        row = []
+        for symb in row_symb:
+            if symb == '#':
+                row.append(1)
+            else:
+                row.append(0)
         data.append(row)
     
     return data
     
 
 #functions for part 1
+#A light which is on stays on when 2 or 3 neighbors are on, and turns off otherwise.
+#A light which is off turns on if exactly 3 neighbors are on, and stays off otherwise.
+def update(grid):
+    new_grid = [[0 for x in grid[0]] for y in grid]
+    count = [[0 for x in grid[0]] for y in grid]
+    end = len(grid) -1
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if i == 0:
+                if j == 0:
+                    count[i][j] = grid[i+1][j] + grid[i][j+1] + grid[i+1][j+1]
+                elif j == end:
+                    count[i][j] = grid[i+1][j] + grid[i][j-1] + grid[i+1][j-1]
+                else:
+                    count[i][j] = grid[i+1][j-1] + grid[i+1][j] + grid[i][j+1] + grid[i+1][j+1] + grid[i][j-1]
+            elif i == end:
+                if j == 0:
+                    count[i][j] = grid[i-1][j] + grid[i][j+1] + grid[i-1][j+1]
+                elif j == end:
+                    count[i][j] = grid[i-1][j] + grid[i][j-1] + grid[i-1][j-1]
+                else:
+                    count[i][j] = grid[i-1][j-1] + grid[i-1][j] + grid[i][j+1] + grid[i-1][j+1] + grid[i][j-1]
+            else:
+                if j == 0:
+                    count[i][j] = grid[i-1][j] + grid[i-1][j+1] + grid[i][j+1] + grid[i+1][j] + grid[i+1][j+1]
+                elif j == end:
+                    count[i][j] = grid[i-1][j-1] + grid[i-1][j] + grid[i][j-1] + grid[i+1][j-1] + grid[i+1][j]
+                else:
+                    count[i][j] = grid[i-1][j-1] + grid[i-1][j] + grid[i-1][j+1] + grid[i][j-1] + grid[i][j+1] + grid[i+1][j-1] + grid[i+1][j] + grid[i+1][j+1]
+            if grid[i][j] == 1:
+                if count[i][j] == 2 or count[i][j] == 3:
+                    new_grid[i][j] = 1
+            else:
+                if count[i][j] == 3:
+                    new_grid[i][j] = 1
+                    
+    return new_grid
 
 #solve part 1
 def part1(puzzle_data):
+    
     return 0
 
 #functions for part 2
