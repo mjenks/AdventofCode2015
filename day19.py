a39d2c1f9b5c51095d9caaa5e128916d9a2b70ed
mjenks <rikku.hotaru@gmail.com>
2022-02-06 19:32:29 -0500

    set up
    set up code for day 19 puzzles

---
+++ day19.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 06 19:31:15 2022
+
+@author: mjenks
+"""
+
+#parse input
+def parse(puzzle_input):
+    data = []
+    return data
+    
+
+#functions for part 1
+
+#solve part 1
+def part1(puzzle_data):
+    return 0
+
+#functions for part 2
+
+#solve part 2
+def part2(puzzle_data):
+    return 0
+
+#run and print solution 
+puzzle_path = "input_day19.txt"
+with open(puzzle_path) as f:
+    puzzle_input = f.readlines()
+    
+puzzle_data = parse(puzzle_input)
+solution1 = part1(puzzle_data)
+solution2 = part2(puzzle_data)
+print(solution1)
+print(solution2)
