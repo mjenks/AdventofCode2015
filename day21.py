eafffff8bab15e431f38ee91d2610b8b1d8794ad
mjenks <rikku.hotaru@gmail.com>
2022-02-10 11:59:58 -0500

    input
    added boss information input and parsed into a dictionary

--- day21.py
+++ day21.py
@@ -8,7 +8,13 @@ Created on Thu Feb 10 11:47:11 2022
 #parse input
 def parse(puzzle_input):
     data = []
-    return data
+    for line in puzzle_input:
+        line = line.split(':')
+        name = line[0].strip()
+        val = line[1].strip()
+        stat = name, val
+        data.append(stat)
+    return dict(data)
     
 
 #functions for part 1
@@ -24,9 +30,9 @@ def part2(puzzle_data):
     return 0
 
 #run and print solution 
-puzzle_path = ""
+puzzle_path = "input_day21.txt"
 with open(puzzle_path) as f:
-    puzzle_input = f.read()
+    puzzle_input = f.readlines()
     
 puzzle_data = parse(puzzle_input)
 solution1 = part1(puzzle_data)
