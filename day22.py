509322cfad24be993574b13eb44d90622b4f39bb
mjenks <rikku.hotaru@gmail.com>
2022-02-11 22:31:40 -0500

    set up
    framework for day 22 puzzle

---
+++ day22.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 11 22:30:48 2022
+
+@author: mjenks
+"""
+
+#parse input
+def parse(puzzle_input):
+	data = []
+	return data
+    
+
+#functions for part 1
+
+#solve part 1
+def part1(puzzle_data):
+	return 0
+
+#functions for part 2
+
+#solve part 2
+def part2(puzzle_data):
+	return 0
+
+#run and print solution 
+puzzle_path = "input_day22.txt"
+with open(puzzle_path) as f:
+    puzzle_input = f.readlines()
+    
+puzzle_data = parse(puzzle_input)
+solution1 = part1(puzzle_data)
+solution2 = part2(puzzle_data)
+print(solution1)
+print(solution2)
