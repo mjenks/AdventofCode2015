123a62edec21355447d46de0039156fb9faf4e1e
mjenks <rikku.hotaru@gmail.com>
2022-02-14 14:09:00 -0500

    part 1
    solution to part 1 gives the smallest product of all groups of 6 that
    have a 508 weight

--- day24.py
+++ day24.py
@@ -5,6 +5,8 @@ Created on Mon Feb 14 12:37:29 2022
 @author: mjenks
 """
 
+import itertools
+
 #parse input
 def parse(puzzle_input):
     data = []
@@ -15,10 +17,30 @@ def parse(puzzle_input):
     
 
 #functions for part 1
+def entangle(group):
+    qe = 1
+    for w in group:
+        qe = qe*w
+    return qe
+
 
 #solve part 1
 def part1(puzzle_data):
-    return 0
+    total_weight = sum(puzzle_data)
+    group_weight = total_weight/3
+    options = set() #will hold all the groups of gifts with the correct weight
+    min_grp = group_weight//puzzle_data[-1]
+    for i in range(min_grp,len(puzzle_data)//3): #smallest group can't have more than 1/3 
+        for j in itertools.combinations(puzzle_data, i):
+            if sum(j) == group_weight:
+                options.add(j)
+    smallest_group = min(len(x) for x in options)
+    qe = []
+    for group in options:
+        if len(group) == smallest_group:
+            qe.append(entangle(group))
+            
+    return min(qe)
 
 #functions for part 2
 
