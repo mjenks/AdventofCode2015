7539e751aa5885c58600a95ffd6cc0adf7fbf3d6
mjenks <rikku.hotaru@gmail.com>
2022-02-15 11:21:40 -0500

    solved
    part 1 solved, no additional code needed for part 2

--- day25.py
+++ day25.py
@@ -6,21 +6,24 @@ Created on Tue Feb 15 10:37:48 2022
 """
 
 #functions for part 1
+def next_code(code):
+    return (code * 252533) % 33554393
+    
+def code_num(loc):
+    row, col = loc
+    number = sum(range(1,col+1)) + sum(range(col,col+row-1))
+    return number
 
 #solve part 1
 def part1(puzzle_data):
-    return 0
+    code = 20151125
+    for i in range(1,code_num(puzzle_data)):
+        code = next_code(code)
+    return code
 
-#functions for part 2
-
-#solve part 2
-def part2(puzzle_data):
-    return 0
 
 #run and print solution 
 puzzle_data = (2947, 3029) # (row, column) from my puzzle input
 
 solution1 = part1(puzzle_data)
-solution2 = part2(puzzle_data)
 print(solution1)
-print(solution2)
